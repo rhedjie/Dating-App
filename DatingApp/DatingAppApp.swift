569fb6a9a734993de6ba8df318ff8ec257a1d26b
Rhedjie Cruz <cruzrhedjie@gmail.com>
2021-03-29 12:16:33 +0800

    Initial Commit

---
+++ DatingApp/DatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppApp.swift
+//  DatingApp
+//
+//  Created by Rhedjie Cruz on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct DatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
